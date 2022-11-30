d23e6950ece966a9cc39a5e753780c771455c028
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-30 20:22:21 +0100

    green: given that I have chosen paper when the opponent chooses scissors then the opponent should win

--- tests/unit/rock-paper-scisssors.spec.ts
+++ tests/unit/rock-paper-scisssors.spec.ts
@@ -38,6 +38,7 @@ describe('Rock paper scissors game', () => {
         [Moves.PAPER, Moves.ROCK, GameWinner.PLAYER_1],
         [Moves.ROCK, Moves.PAPER, GameWinner.PLAYER_2],
         [Moves.SCISSORS, Moves.PAPER, GameWinner.PLAYER_1],
+        [Moves.PAPER, Moves.SCISSORS, GameWinner.PLAYER_2],
     ])('when I play %s and the opponent plays %s the %s', (myMove: Moves, opponentMove: Moves, expectedWinner: GameWinner) => {
         const actualWinner = rockPaperScissors(myMove, opponentMove);
         expect(actualWinner).toBe(expectedWinner);
