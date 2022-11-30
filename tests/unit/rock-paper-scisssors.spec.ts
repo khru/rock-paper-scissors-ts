67ad87fec2759d6aa0a2fdfffb4fd72911fea6d1
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-30 20:20:17 +0100

    refactor: unify test and completed the use cases

--- tests/unit/rock-paper-scisssors.spec.ts
+++ tests/unit/rock-paper-scisssors.spec.ts
@@ -20,11 +20,11 @@ function rockPaperScissors(movePlayer1: Moves, movePlayer2: Moves) {
         return GameWinner.PLAYER_1;
     }
 
-    if (movePlayer1 === Moves.ROCK) {
+    if (movePlayer1 === Moves.ROCK && movePlayer2 === Moves.SCISSORS) {
         return GameWinner.PLAYER_1;
     }
 
-    if (movePlayer1 === Moves.PAPER) {
+    if (movePlayer1 === Moves.PAPER && movePlayer2 === Moves.ROCK) {
         return GameWinner.PLAYER_1;
     }
 
@@ -37,14 +37,9 @@ describe('Rock paper scissors game', () => {
         [Moves.SCISSORS, Moves.ROCK, GameWinner.PLAYER_2],
         [Moves.PAPER, Moves.ROCK, GameWinner.PLAYER_1],
         [Moves.ROCK, Moves.PAPER, GameWinner.PLAYER_2],
+        [Moves.SCISSORS, Moves.PAPER, GameWinner.PLAYER_1],
     ])('when I play %s and the opponent plays %s the %s', (myMove: Moves, opponentMove: Moves, expectedWinner: GameWinner) => {
         const actualWinner = rockPaperScissors(myMove, opponentMove);
-        expect(actualWinner).toBe(expectedWinner);
-    });
-
-    it('given that I have chosen scissors when the opponent chooses paper then I should win', () => {
-        const actualWinner = rockPaperScissors(Moves.SCISSORS, Moves.PAPER);
-        const expectedWinner = GameWinner.PLAYER_1;
         expect(actualWinner).toBe(expectedWinner);
     });
 });
