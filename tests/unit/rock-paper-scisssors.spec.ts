a6752ca8e6bdc2e458e7f07a17b377f5cfd26b23
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-30 18:29:17 +0100

    red: given that I have chosen rock when the opponent chooses scissors then I should win - test failing for the right reason

--- tests/unit/rock-paper-scisssors.spec.ts
+++ tests/unit/rock-paper-scisssors.spec.ts
@@ -1,3 +1,6 @@
+function rockPaperScissors(movePlayer1: string, movePlayer2: string) {
+}
+
 describe('Rock paper scissors game', () => {
     it('given that I have chosen rock when the opponent chooses scissors then I should win', () => {
         expect(rockPaperScissors("rock","scissors")).toBe('Player 1');
