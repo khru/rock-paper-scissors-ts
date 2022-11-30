6236099caf24823ad907bc2e43f893e9dd0ecf06
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-30 19:52:32 +0100

    refactor: extract moves to enum, extract players to enum create a describe for a use case

--- tests/unit/rock-paper-scisssors.spec.ts
+++ tests/unit/rock-paper-scisssors.spec.ts
@@ -1,31 +1,46 @@
-function rockPaperScissors(movePlayer1: string, movePlayer2: string) {
-    if (movePlayer1 == 'rock') {
-        return 'Player 1';
+
+enum Players {
+    PLAYER_1,
+    PLAYER_2
+}
+
+enum Moves {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
+
+function rockPaperScissors(movePlayer1: Moves, movePlayer2: Moves) {
+    if (movePlayer1 == Moves.ROCK) {
+        return Players.PLAYER_1;
     }
 
-    if (movePlayer1 === 'paper') {
-        return 'Player 1';
+    if (movePlayer1 === Moves.PAPER) {
+        return Players.PLAYER_1;
     }
 
-    return 'Player 2';
+    return Players.PLAYER_2;
 }
 
 describe('Rock paper scissors game', () => {
-    it('given that I have chosen rock when the opponent chooses scissors then I should win', () => {
-        const actualWinner = rockPaperScissors('rock','scissors');
-        const expectedWinner = 'Player 1';
-        expect(actualWinner).toBe(expectedWinner);
-    });
 
-    it('given that I have chosen scissors when the opponent chooses rock then the opponent should win', () => {
-        const actualWinner = rockPaperScissors('scissors','rock');
-        const expectedWinner = 'Player 2';
-        expect(actualWinner).toBe(expectedWinner);
+    describe('Rock beats Scissors', () => {
+        it('given that I have chosen rock when the opponent chooses scissors then I should win', () => {
+            const actualWinner = rockPaperScissors(Moves.ROCK, Moves.SCISSORS);
+            const expectedWinner = Players.PLAYER_1;
+            expect(actualWinner).toBe(expectedWinner);
+        });
+
+        it('given that I have chosen scissors when the opponent chooses rock then the opponent should win', () => {
+            const actualWinner = rockPaperScissors(Moves.SCISSORS, Moves.ROCK);
+            const expectedWinner = Players.PLAYER_2;
+            expect(actualWinner).toBe(expectedWinner);
+        });
     });
 
     it('given that I have chosen paper when the opponent chooses rock then I should win', () => {
-        const actualWinner = rockPaperScissors('paper','rock');
-        const expectedWinner = 'Player 1';
+        const actualWinner = rockPaperScissors(Moves.PAPER, Moves.ROCK);
+        const expectedWinner = Players.PLAYER_1;
         expect(actualWinner).toBe(expectedWinner);
     });
 });
