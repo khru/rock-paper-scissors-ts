b81559da71a53a929cffbcc4fe0efece1b503c9d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-30 20:30:23 +0100

    red: given that I have chosen rock when the opponent chooses rock then it should be a draw

--- tests/unit/rock-paper-scisssors.spec.ts
+++ tests/unit/rock-paper-scisssors.spec.ts
@@ -1,7 +1,8 @@
 
 enum GameWinner {
     PLAYER_1 = 'Player 1',
-    PLAYER_2 = 'Player 2'
+    PLAYER_2 = 'Player 2',
+    DRAW = "Draw",
 }
 
 enum Moves {
@@ -34,6 +35,7 @@ describe('Rock paper scissors game', () => {
         [Moves.ROCK, Moves.PAPER, GameWinner.PLAYER_2],
         [Moves.SCISSORS, Moves.PAPER, GameWinner.PLAYER_1],
         [Moves.PAPER, Moves.SCISSORS, GameWinner.PLAYER_2],
+        [Moves.ROCK, Moves.ROCK, GameWinner.DRAW],
     ])('when I play %s and the opponent plays %s the %s', (myMove: Moves, opponentMove: Moves, expectedWinner: GameWinner) => {
         const actualWinner = rockPaperScissors(myMove, opponentMove);
         expect(actualWinner).toBe(expectedWinner);
