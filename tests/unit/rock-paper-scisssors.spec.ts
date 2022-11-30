fc9543be2cba194a3f66a38b64578ad24691ce18
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-30 18:34:36 +0100

    refactor: create a more descriptive variables

--- tests/unit/rock-paper-scisssors.spec.ts
+++ tests/unit/rock-paper-scisssors.spec.ts
@@ -4,6 +4,8 @@ function rockPaperScissors(movePlayer1: string, movePlayer2: string) {
 
 describe('Rock paper scissors game', () => {
     it('given that I have chosen rock when the opponent chooses scissors then I should win', () => {
-        expect(rockPaperScissors("rock","scissors")).toBe('Player 1');
+        const actualWinner = rockPaperScissors("rock","scissors");
+        const expectedWinner = 'Player 1';
+        expect(actualWinner).toBe(expectedWinner);
     });
 });
